b2b485688434298d501904fd506bb6f827730e46
Lea Maida-Lombardi <lea.maida-lombardi@etu.univ-grenoble-alpes.fr>
2023-01-13 11:01:04 +0100

    babystep done

--- src/baby_step/Client.java
+++ src/baby_step/Client.java
@@ -1,7 +1,49 @@
 package baby_step;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 	public static void main(String[] args) {
+		String host = "localhost";
+		int port = 9999;
+		Socket soc = null;
+
+		try {
+			soc = new Socket(host, port);
+
+			String name = args[0];
+			if (name == null) {
+				Scanner scanner = new Scanner(System.in);
+				System.out.print("Entrez votre nom: ");
+				name = scanner.nextLine();
+			}
+
+			OutputStream os = soc.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+			dos.writeUTF(name);
+
+			InputStream is = soc.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			String message = dis.readUTF();
+
+			System.out.println(message);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				soc.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 }
--- src/baby_step/Server.java
+++ src/baby_step/Server.java
@@ -1,7 +1,43 @@
 package baby_step;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
 	public static void main(String[] args) {
+
+		int serverPort = 9999;
+		int backlog = 3;
+		ServerSocket listenSoc = null;
+
+		try {
+			listenSoc = new ServerSocket(serverPort, backlog);
+			System.out.println("serveur demarre sur le port " + serverPort);
+			while (true) {
+				Socket soc = listenSoc.accept();
+				InputStream is = soc.getInputStream();
+				DataInputStream dis = new DataInputStream(is);
+				String name = dis.readUTF();
+				OutputStream os = soc.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(os);
+				dos.writeUTF("Hello " + name);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				listenSoc.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 }
